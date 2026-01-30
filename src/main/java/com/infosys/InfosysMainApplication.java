8b0857e6a9ac922d5b50bea2747329395cc0ab8e
sharif <nagulsmail7@gmail.com>
2026-01-31 00:12:29 +0530

    Maded project setup

--- src/main/java/com/infosys/InfosysMainApplication.java
+++ src/main/java/com/infosys/InfosysMainApplication.java
@@ -1,10 +1,11 @@
 package com.infosys;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class InfosysMainApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
